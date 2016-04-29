93ec391479910b004082c933681a9b4ec57b5240
Steve Henderson <hevets@gmail.com>
2016-04-28 23:14:47 -0700

    Initial project setup, adding two viewcontrollers with basic UI elements

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
+    
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        toggleRecording(true)
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop record button was pressed")
+        toggleRecording(false)
+    }
+    
+    /** 
+    Toggles recording and stoprecording button states
+    Updates recording state text on recordingLabel
+    */
+    func toggleRecording(isRecording: Bool) {
+        recordButton.enabled = !isRecording
+        stopRecordingButton.enabled = isRecording
+        recordingLabel.text = isRecording ? "Recording in Progress" : "Tap to Record"
+    }
 }
 
